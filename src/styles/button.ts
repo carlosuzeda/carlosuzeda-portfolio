9f68bf32d27d287889c72e7e1c51c81422f0ae72
carlosuzeda <carlosuzeda.dev@gmail.com>
2023-04-27 10:34:56 -0300

    fix style button

--- src/styles/button.ts
+++ src/styles/button.ts
@@ -1,36 +1,45 @@
 import { styled } from ".";
 
-export const Button = styled('button',{
+export const Button = styled('div',{
+  a:{
+    display: 'block',
     border: '1px solid transparent',
+    textAlign: 'center',
     borderRadius: 8,
     padding: '1rem',
    
     cursor: 'pointer',
     transition: 'all .2s',
-    a:{
+
       textDecoration: 'none',
       color: '$white'
     },
     variants: {
         color: {
           gray: {
-            backgroundColor: '$gray-800',
-            color: 'white',
-            fontSize: 16,
-            width: '100%',
-            '&:hover': {
-              backgroundColor: '$gray-900',
-              border: '1px solid $gray-800',
-            },
+            a:{
+              backgroundColor: '$gray-800',
+              color: 'white',
+              fontSize: 16,
+              width: '100%',
+              '&:hover': {
+                backgroundColor: '$gray-900',
+                border: '1px solid $gray-800',
+              },
+            }
+         
           },
           black: {
-            backgroundColor: '$gray-900',
-            color: 'white',
-            fontSize: 16,
-            '&:hover': {
-              backgroundColor: '$gray-800',
-              border: '1px solid $gray-900',
-            },
+            a:{
+              backgroundColor: '$gray-900',
+              color: 'white',
+              fontSize: 16,
+              '&:hover': {
+                backgroundColor: '$gray-800',
+                border: '1px solid $gray-900',
+              },
+            }
+          
           },
         }
     }
